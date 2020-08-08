1697b4de3ecf0f1a406ae654a0d03a6df9a96668
sam1370 <sampiano@outlook.com>
2020-08-08 16:30:26 +0100

    Forgot to only respond to !

--- index.js
+++ index.js
@@ -9,61 +9,63 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	var args = message.content.substring(1).split(' ');
-	var cmd = args[0];
-	args = args.splice(1);
-	var channel = message.channel;
-	switch (cmd) {
-		case 'help':
-			channel.send('Available commands:\n`!pdir` gives the panel directory\n`!logs` shows how to retrieve logs for the panel and daemon\n`!install` gives the master installation script\n`!check` gives a useful command to check for panel errors\n`!firewall` gives a link to directions on firewall setup\n`!nstart`, `!nrestart`, `!nstop`, and `!nstatus` all give instructions on how to perform those respective actions to nginx using systemctl\n`!wstart`, `!wrestart`, `!wstop`, and `!wstatus` perform similar functions as above except for wings\n`!config` provides a link to the official instructions on how to manually configure the daemon\n`!storage` provides the location in which server files are stored\n`!muser` gives instructions on how to create a new user for the panel');
-			break;
-		case 'pdir':
-			channel.send('Default panel directory is `/var/www/pterodactyl/`.');
-			break;
-		case 'logs':
-			channel.send('Panel logs: `tail -n 100 /var/www/pterodactyl/storage/logs/laravel-$(date +%F).log | nc bin.ptdl.co 99`\nDaemon logs: `cd /srv/daemon/ && npm run diagnostics`');
-			break;
-		case 'install':
-			channel.send('Install with:\n```bash <(curl -s https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer/master/install.sh)```\nIf this fails, it is possible to do the same thing by running this:\n```wget https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer/master/install.sh\nbash install.sh```');
-			break;
-		case 'check':
-			channel.send('This command can be useful to check for errors on the panel:\n`/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3`');
-			break;
-		case 'firewall':
-			channel.send('The installation scripts do not configure your firewall automatically. Here are the instructions for how to do so: <https://github.com/vilhelmprytz/pterodactyl-installer#firewall-setup>');
-			break;
-		case 'nstart':
-			channel.send('Start nginx with `systemctl start nginx`');
-			break;
-		case 'nrestart':
-			channel.send('Restart nginx with `systemctl restart nginx`');
-			break;
-		case 'nstatus':
-			channel.send('Check status and logs of nginx with `systemctl status nginx`');
-			break;
-		case 'nstop':
-			channel.send('Stop nginx with `systemctl stop nginx`');
-			break;
-		case 'wstart':
-			channel.send('Start the daemon after configuration with `systemctl start wings`');
-			break;
-		case 'wrestart':
-			channel.send('Restart the daemon with `systemctl restart wings`');
-			break;
-		case 'wstatus':
-			channel.send('Check status and logs of the daemon with `systemctl status wings`');
-			break;
-		case 'wstop':
-			channel.send('Stop the daemon with `systemctl stop wings`');
-			break;
-		case 'config':
-			channel.send('The guide to configuring your daemon can be found here: <https://pterodactyl.io/daemon/0.6/installing.html#configure-daemon>');
-			break;
-		case 'storage':
-			channel.send('The server files on the daemon are stored:\nnormally: in `/srv/daemon-data/`\non beta 1.0: in `/var/lib/pterodactyl/volumes/`');
-			break;
-		case 'muser':
-			channel.send('Create a new panel user by running: ```cd /var/www/pterodactyl/\nphp artisan p:user:make```');
-			break;
+	if (message.content.substring(0, 1) == '!') {
+		var args = message.content.substring(1).split(' ');
+		var cmd = args[0];
+		args = args.splice(1);
+		var channel = message.channel;
+		switch (cmd) {
+			case 'help':
+				channel.send('Available commands:\n`!pdir` gives the panel directory\n`!logs` shows how to retrieve logs for the panel and daemon\n`!install` gives the master installation script\n`!check` gives a useful command to check for panel errors\n`!firewall` gives a link to directions on firewall setup\n`!nstart`, `!nrestart`, `!nstop`, and `!nstatus` all give instructions on how to perform those respective actions to nginx using systemctl\n`!wstart`, `!wrestart`, `!wstop`, and `!wstatus` perform similar functions as above except for wings\n`!config` provides a link to the official instructions on how to manually configure the daemon\n`!storage` provides the location in which server files are stored\n`!muser` gives instructions on how to create a new user for the panel');
+				break;
+			case 'pdir':
+				channel.send('Default panel directory is `/var/www/pterodactyl/`.');
+				break;
+			case 'logs':
+				channel.send('Panel logs: `tail -n 100 /var/www/pterodactyl/storage/logs/laravel-$(date +%F).log | nc bin.ptdl.co 99`\nDaemon logs: `cd /srv/daemon/ && npm run diagnostics`');
+				break;
+			case 'install':
+				channel.send('Install with:\n```bash <(curl -s https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer/master/install.sh)```\nIf this fails, it is possible to do the same thing by running this:\n```wget https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer/master/install.sh\nbash install.sh```');
+				break;
+			case 'check':
+				channel.send('This command can be useful to check for errors on the panel:\n`/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3`');
+				break;
+			case 'firewall':
+				channel.send('The installation scripts do not configure your firewall automatically. Here are the instructions for how to do so: <https://github.com/vilhelmprytz/pterodactyl-installer#firewall-setup>');
+				break;
+			case 'nstart':
+				channel.send('Start nginx with `systemctl start nginx`');
+				break;
+			case 'nrestart':
+				channel.send('Restart nginx with `systemctl restart nginx`');
+				break;
+			case 'nstatus':
+				channel.send('Check status and logs of nginx with `systemctl status nginx`');
+				break;
+			case 'nstop':
+				channel.send('Stop nginx with `systemctl stop nginx`');
+				break;
+			case 'wstart':
+				channel.send('Start the daemon after configuration with `systemctl start wings`');
+				break;
+			case 'wrestart':
+				channel.send('Restart the daemon with `systemctl restart wings`');
+				break;
+			case 'wstatus':
+				channel.send('Check status and logs of the daemon with `systemctl status wings`');
+				break;
+			case 'wstop':
+				channel.send('Stop the daemon with `systemctl stop wings`');
+				break;
+			case 'config':
+				channel.send('The guide to configuring your daemon can be found here: <https://pterodactyl.io/daemon/0.6/installing.html#configure-daemon>');
+				break;
+			case 'storage':
+				channel.send('The server files on the daemon are stored:\nnormally: in `/srv/daemon-data/`\non beta 1.0: in `/var/lib/pterodactyl/volumes/`');
+				break;
+			case 'muser':
+				channel.send('Create a new panel user by running: ```cd /var/www/pterodactyl/\nphp artisan p:user:make```');
+				break;
+		}
 	}
 });
