954e2280d660954651860c50bc00ef790313f662
sam1370 <sampiano@outlook.com>
2020-08-14 17:25:31 +0100

    Use tabs

--- index.js
+++ index.js
@@ -6,64 +6,64 @@ let triggers = require("./triggers.json");
 client.login(auth.token);
 
 client.once("ready", () => {
-  console.log(
-    "Logged in as: " + client.user.username + " - (" + client.user.id + ")"
-  );
-  client.user.setActivity("!help");
+	console.log(
+		"Logged in as: " + client.user.username + " - (" + client.user.id + ")"
+	);
+	client.user.setActivity("!help");
 });
 
 client.on("message", (message) => {
-  if (message.author.id === client.user.id) return;
-  var msgContent = message.content.toLowerCase();
-  var channel = message.channel;
-  var isCommand = false;
-  if (msgContent.substring(0, 1) == "!") {
-    var args = msgContent.substring(1).split(" ");
-    var cmd = args[0];
-    args = args.splice(1);
-    isCommand = true;
-  }
-  if (isCommand) {
-    for (var i = 0; i < triggers.length; i++) {
-      var matched = false;
-      var trig = triggers[i];
-      if (trig.hasOwnProperty("cmd")) {
-        cmdtomatch = trig.cmd.toLowerCase();
-        if (cmd === cmdtomatch) {
-          matched = true;
-        }
-      }
-      if (trig.hasOwnProperty("aliases") && matched == false) {
-        var aliases = trig.aliases;
-        for (var g = 0; g < aliases.length; g++) {
-          aliastomatch = aliases[g].toLowerCase();
-          if (cmd === aliastomatch) {
-            matched = true;
-            break;
-          }
-        }
-      }
-      if (matched == true) {
-        channel.send(trig.lines.join("\n"));
-      }
-    }
-  } else {
-    for (var i = 0; i < triggers.length; i++) {
-      var matched = false;
-      var trig = triggers[i];
-      if (trig.hasOwnProperty("keys")) {
-        var keys = trig.keys;
-        for (var h = 0; h < keys.length; h++) {
-          keytomatch = keys[h].toLowerCase();
-          if (msgContent.includes(keytomatch)) {
-            matched = true;
-            break;
-          }
-        }
-      }
-      if (matched == true) {
-        channel.send(trig.lines.join("\n"));
-      }
-    }
-  }
+	if (message.author.id === client.user.id) return;
+	var msgContent = message.content.toLowerCase();
+	var channel = message.channel;
+	var isCommand = false;
+	if (msgContent.substring(0, 1) == "!") {
+		var args = msgContent.substring(1).split(" ");
+		var cmd = args[0];
+		args = args.splice(1);
+		isCommand = true;
+	}
+	if (isCommand) {
+		for (var i = 0; i < triggers.length; i++) {
+			var matched = false;
+			var trig = triggers[i];
+			if (trig.hasOwnProperty("cmd")) {
+				cmdtomatch = trig.cmd.toLowerCase();
+				if (cmd === cmdtomatch) {
+					matched = true;
+				}
+			}
+			if (trig.hasOwnProperty("aliases") && matched == false) {
+				var aliases = trig.aliases;
+				for (var g = 0; g < aliases.length; g++) {
+					aliastomatch = aliases[g].toLowerCase();
+					if (cmd === aliastomatch) {
+						matched = true;
+						break;
+					}
+				}
+			}
+			if (matched == true) {
+				channel.send(trig.lines.join("\n"));
+			}
+		}
+	} else {
+		for (var i = 0; i < triggers.length; i++) {
+			var matched = false;
+			var trig = triggers[i];
+			if (trig.hasOwnProperty("keys")) {
+				var keys = trig.keys;
+				for (var h = 0; h < keys.length; h++) {
+					keytomatch = keys[h].toLowerCase();
+					if (msgContent.includes(keytomatch)) {
+						matched = true;
+						break;
+					}
+				}
+			}
+			if (matched == true) {
+				channel.send(trig.lines.join("\n"));
+			}
+		}
+	}
 });
