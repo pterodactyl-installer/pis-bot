eb23daa755598524391671954c89ec99d8a89399
Linux123123 <linas.alexx@gmail.com>
2021-03-02 16:55:28 +0200

    Also replace config in src folder

--- src/setup.ts
+++ src/setup.ts
@@ -4,6 +4,10 @@ import config from './config/config';
 
 if (config.token !== 'TOKEN' || config.prefix !== 'PREFIX') process.exit(0);
 let baseConfig = fs.readFileSync(`${__dirname}/config/config.js`, 'utf8');
+let baseSrcConfig = fs.readFileSync(
+    `${__dirname}/../src/config/config.ts`,
+    'utf8',
+);
 
 console.log('First Start!');
 console.log('Setting Up Configuration...');
@@ -14,8 +18,11 @@ console.log('Enter your prefered prefix for the bot (!): ');
 const PREFIX = reader.question('', { defaultInput: '!' });
 
 baseConfig = baseConfig.replace('TOKEN', `${TOKEN}`);
+baseSrcConfig = baseSrcConfig.replace('TOKEN', `${TOKEN}`);
 baseConfig = baseConfig.replace('PREFIX', `${PREFIX}`);
+baseSrcConfig = baseSrcConfig.replace('PREFIX', `${PREFIX}`);
 
 fs.writeFileSync(`${__dirname}/config/config.js`, baseConfig);
+fs.writeFileSync(`${__dirname}/../src/config/config.ts`, baseSrcConfig);
 console.log('REMEMBER TO NEVER SHARE YOUR TOKEN WITH ANYONE!');
 console.log('Configuration has been written, enjoy!');
